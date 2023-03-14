da004d7b3410499d1c949594f0ed6a1a5a1662a3
Sieger06 <irek.haibulin@gmail.com>
2023-03-14 18:54:54 +0300

    done

--- src/main/java/pro/sky/maven/controllers/firstController.java
+++ src/main/java/pro/sky/maven/controllers/firstController.java
@@ -1,2 +1,20 @@
-package pro.sky.maven.controllers;public class firstController {
+package pro.sky.maven.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class firstController {
+
+    @GetMapping()
+    public String startPage (){
+        return "Приложение запужено";
+    }
+    @GetMapping("/info")
+    public String introProject(){
+        return "Ф.И.О.: Хайбуллин И.М.}; " +
+                "Название проекта: 'Maven'; " +
+                "Дата создания проекта: 14.03.2023; " +
+                "Описание проекта: Создание web-приложения" ;
+    }
 }
